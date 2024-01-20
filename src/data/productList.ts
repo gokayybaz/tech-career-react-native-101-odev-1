30054bf365050ef2870517877d840d6720d6d4a1
gokayybaz <gokaybaz2000@outlook.com>
2024-01-20 12:00:31 +0300

    odev-tamamlandi

---
+++ src/data/productList.ts
@@ -0,0 +1,50 @@
+export const productList = [
+  {
+    id: 1,
+    name: 'Asus Laptop',
+    image:
+      'https://dlcdnwebimgs.asus.com/gain/91f5e32d-e415-4d8b-a080-ffadc04fceee/',
+    description: 'Asus L210',
+    unitPrice: 18500,
+  },
+  {
+    id: 2,
+    name: 'MSI Laptop',
+    image:
+      'https://www.gaming.gen.tr/wp-content/uploads/2023/05/msi-stealth-16-studio-a13vf-071tr-intel-core-i7-13700h-16gb-ddr5-8gb-1tb-ssd-rtx4060-16-inc-qhd-w11h-gaming-laptop_0001_-1.jpg',
+    description: 'MSI STEALTH 16 STUDIO',
+    unitPrice: 35000,
+  },
+  {
+    id: 3,
+    name: 'Dell Laptop',
+    image:
+      'https://www.ubuy.com.tr/productimg/?image=aHR0cHM6Ly9tLm1lZGlhLWFtYXpvbi5jb20vaW1hZ2VzL0kvNjEteWxuNElYakwuX0FDX1NMMTUwMF8uanBn.jpg',
+    description: 'Dell Inspiron 15',
+    unitPrice: 19500,
+  },
+  {
+    id: 4,
+    name: 'Apple Macbook Air',
+    image:
+      'https://m.media-amazon.com/images/I/71jG+e7roXL._AC_UF1000,1000_QL80_.jpg',
+    description: 'Asus Macbook Air M1 8GB 256GB',
+    unitPrice: 24500,
+  },
+  {
+    id: 5,
+    name: 'Acer Laptop',
+    image:
+      'https://m.media-amazon.com/images/I/716U4i2woML._AC_UF1000,1000_QL80_.jpg',
+    description: 'Acer Aspire 5',
+    unitPrice: 15500,
+  },
+  {
+    id: 6,
+    name: 'Monster Laptop',
+    image:
+      'https://cdn.akakce.com/x/monster/monster-abra-a5-v17-4-4-i7-11800h-32-gb-1-tb-ssd-rtx3060-15-6-full-hd-notebook.jpg',
+    description: 'Monster Abra A5',
+    unitPrice: 32500,
+  },
+];
---
+++ src/screens/ProductListScreen.tsx
@@ -0,0 +1,27 @@
+import {ScrollView} from 'react-native';
+import {Card, Text} from 'react-native-paper';
+import React from 'react';
+import {productList} from '../data/productList';
+
+const ProductListScreen = () => {
+  return (
+    <ScrollView style={{paddingLeft: '5%', paddingRight: '5%'}}>
+      {productList.map(item => (
+        <Card style={{marginTop: '5%'}} key={item.id}>
+          <Card.Cover style={{padding: '2%'}} source={{uri: item.image}} />
+          <Card.Content style={{marginTop: '5%'}}>
+            <Text variant="titleLarge">{item.name}</Text>
+            <Text style={{marginTop: '2%'}} variant="titleLarge">
+              Fiyat : {item.unitPrice}
+            </Text>
+            <Text style={{marginTop: '2%'}} variant="bodyMedium">
+              Özellikler : {item.description}
+            </Text>
+          </Card.Content>
+        </Card>
+      ))}
+    </ScrollView>
+  );
+};
+
+export default ProductListScreen;
